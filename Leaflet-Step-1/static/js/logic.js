e76d242ff041bc4d7462c2a0b5156a665ae1a8e3
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-07-18 22:09:05 -0700

    initial commit

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,6 @@
+
+//go to USGS geojson feed and pick data set
+//create a map that plots all of the earthquakes based on lat/long
+//depth of earth can be found as the thir coord of each earthquake
+//include popups that provide additional infor when a marker is clicked
+//create a legend
